f1d734c8025af26e57938964ffa57351c679527b
Ivan Trajanovski <ivan.trajanovski@egroup.hu>
2022-02-18 16:01:08 +0100

    create env variables for apis

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,7 @@ export const getPlacesData = async (type, sw, ne) => {
                 },
                 headers: {
                   'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                  'x-rapidapi-key': '2afe66f9bbmsh2abf229b0445b20p12904ejsn2ed5fb675435'
+                  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
                 }
         });
 
@@ -32,7 +32,7 @@ export const getWeatherData = async (lat, lng) => {
       },
       headers: {
         'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': '2afe66f9bbmsh2abf229b0445b20p12904ejsn2ed5fb675435'
+        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
       }
     });
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -14,7 +14,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyD53nW5EMThNiFzTXNrc8RLMqc_kFd4pyE' }}
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
