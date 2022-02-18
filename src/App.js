bc3a3f8d195b49533c313ab8cb522e2c0997af07
Ivan Trajanovski <ivan.trajanovski@egroup.hu>
2022-02-18 15:47:19 +0100

    add search places

--- src/App.js
+++ src/App.js
@@ -31,19 +31,21 @@ const App = () => {
     }, [rating]);
     
     useEffect(() => {
-        setIsLoading(true);
-        getPlacesData(type, bounds.sw, bounds.ne)
-            .then((data) => {
-                setPlaces(data);
-                setFilteredPlaces([]);
-                setIsLoading(false);
-            })
-    }, [type, coordinates, bounds])
+        if(bounds.sw && bounds.ne) {
+            setIsLoading(true);
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setFilteredPlaces([]);
+                    setIsLoading(false);
+                })
+        }
+    }, [type, bounds])
 
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates={setCoordinates} />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
